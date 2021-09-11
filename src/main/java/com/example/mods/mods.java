30479861a6d4b4823dd19996dd8e55201d2fb029
PavelVorontsov2002 <vorontsov.pavel2002@gmail.com>
2021-09-11 22:12:22 +0300

    add file

---
+++ src/main/java/com/example/mods/mods.java
@@ -0,0 +1,12 @@
+package com.example.mods;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+
+@Mod(modid = "Goblin Slayer", name = "First mode", version = "1.0")
+
+public class mods {
+	@EventHandler
+	public void preLoad(){
+	
+	}
+}
